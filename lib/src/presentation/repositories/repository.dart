8158164b138aa4b3ca97f7d59045e2f4f62c44e9
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2024-05-11 19:42:35 +0200

    fix(repositories): Change Repository from mixin to cubit
    Changed the Repository mixin to be a generic abstract class extending from Cubit
    
    Repository is now an abstract class instead of a mixin

--- lib/src/presentation/repositories/repository.dart
+++ lib/src/presentation/repositories/repository.dart
@@ -1,11 +1,32 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logging/logging.dart';
 import 'package:mcquenji_core/src/domain/domain.dart';
 
-/// Mixin for all repositories that provides logging.
-mixin Repository implements ILoggable {
+/// Base class for all repositories.
+///
+/// As repositories are responsible for managing the state of the application, they extend [Cubit].
+abstract class Repository<T> extends Cubit<T> implements ILoggable {
+  /// Base class for all repositories.
+  ///
+  /// As repositories are responsible for managing the state of the application, they extend [Cubit].
+  Repository(super.initialState);
+
   @override
   Level get level => Level.FINEST;
 
   @override
-  String get namespace => "Repositories.$runtimeType";
+  String get namespace => "Repositories";
+
+  @override
+  Level get errorLevel => Level.SEVERE;
+
+  @override
+  void log(Object message, [Object? error, StackTrace? stackTrace]) {
+    Logger("$namespace.$runtimeType").log(
+      error != null ? errorLevel : level,
+      message,
+      error,
+      stackTrace,
+    );
+  }
 }
