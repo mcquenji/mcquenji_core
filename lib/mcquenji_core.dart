aca901270b1461012999d88d5a54240b243976a2
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2024-05-16 17:19:08 +0200

    fix(services): make base options non null

--- lib/mcquenji_core.dart
+++ lib/mcquenji_core.dart
@@ -23,7 +23,7 @@ import 'package:mcquenji_core/src/impl/impl.dart';
 class CoreModule extends Module {
   @override
   void exportedBinds(i) {
-    i.add<BaseOptions>(() => null);
+    i.add<BaseOptions>(() => BaseOptions());
     i.add(Dio.new);
 
     i.addLazySingleton<ConnectivityService>(
